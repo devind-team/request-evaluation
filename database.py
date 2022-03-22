cf6d7836369d6a8e785f577aa4cc1a5b4ec42709
MarionetkaX <sergey07061999@gmail.com>
2022-03-22 17:21:44 +0300

    feat: add conection database

---
+++ database.py
@@ -0,0 +1,24 @@
+from sqlmodel import SQLModel
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost:5432/traffic'
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        # await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(
+        engine, class_=AsyncSession, expire_on_commit=False
+    )
+    async with async_session() as session:
+        yield session
