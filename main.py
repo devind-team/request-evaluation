c38652383c4c33ecbca632508d4e64ed1a4a9f6e
MarionetkaX <sergey07061999@gmail.com>
2022-03-24 14:59:21 +0300

    feat: counter request with add database

--- main.py
+++ main.py
@@ -2,16 +2,14 @@ import asyncio
 from fastapi import Depends, FastAPI
 from fastapi.responses import RedirectResponse
 from sqlalchemy.future import select
+from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from sqlalchemy.dialects.postgresql import insert
 from datetime import date
-
 from database import get_session
-from models import Traffic
+from models import Traffic, TrafficCreate
 
 app = FastAPI()
-counter = 0
-lock = asyncio.Lock()
 
 
 @app.get('/')
@@ -19,19 +17,27 @@ async def redirect_page_docs():
     return RedirectResponse('/docs#/')
 
 
-@app.get('/traffic', response_model=list[Traffic])
+@app.get('/traffic',
+         response_model=list[Traffic])
 async def calculate(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Traffic).where(Traffic.create_at == date.today()))
-    results = result.scalars().first()
-    print(results)
+
+    insert_records = insert(Traffic).values(counter=1,
+                                            create_at=date.today())
+    update_records = insert_records.on_conflict_do_update(constraint='traffic_create_at_key',
+                                                          set_=dict(counter=Traffic.counter + 1))
+    await session.execute(update_records)
+    # await session.execute(update(Traffic).
+    #                       where(Traffic.create_at == date.today()).
+    #                       values(counter=Traffic.counter + 1))
+    await session.commit()
 
 
-
-@app.get("/calculate")
-async def calculate1():
-    global counter
-
-    async with lock:
-        counter += 1
-
-    return {'request': f'{counter}'}
+@app.post('/traffic')
+async def add_traffic(traffic: TrafficCreate,
+                      session: AsyncSession = Depends(get_session)):
+    traffic = Traffic(counter=traffic.counter,
+                      create_at=traffic.create_at)
+    session.add(traffic)
+    await session.commit()
+    await session.refresh(traffic)
+    return traffic
