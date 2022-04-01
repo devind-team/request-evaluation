c5b5f9b244429d1d100ee4962a0e3b3e98812936
MarionetkaX <sergey07061999@gmail.com>
2022-04-01 11:33:08 +0300

    fix: generate word file via template

--- generate_word/generate_file.py
+++ generate_word/generate_file.py
@@ -2,22 +2,21 @@ from os.path import join
 from settings import get_settings
 from docxtpl import DocxTemplate
 from datetime import date, timedelta
-from sqlalchemy.future import select
-from sqlalchemy.ext.asyncio import AsyncSession
-from database import get_session
-from models import Traffic
-from fastapi import Depends
+
+CURRENT_DATE = date.today() - timedelta(days=1)
+PATH_REPORT = join(get_settings().static_dir, f'{CURRENT_DATE}.docx')
 
 
-async def create_report(traffic: Traffic, session: AsyncSession = Depends(get_session)):
+def create_report(counter, avg_load, max_load):
     template_word = DocxTemplate(join(get_settings().template_dir, 'report.docx'))
     template_word.render(
         {
-            'date': f'{date.today().day}.{date.today().month}.{date.today().year}',
-            'counter': (await session.execute(
-                select(traffic).
-                    where(traffic.create_at == date.today()-timedelta(days=1)))
-                        ).scalar().counter
+            'date': f'{CURRENT_DATE.day}.{CURRENT_DATE.month}.{CURRENT_DATE.year}',
+            'counter': counter,
+            'avg_load': avg_load,
+            'max_load': max_load
+
         }
     )
-    await template_word.save(join(get_settings().static_dir, f'{date.today()}.docx'))
+    template_word.save(PATH_REPORT)
+    return PATH_REPORT
