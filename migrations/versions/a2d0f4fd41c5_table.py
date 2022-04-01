86bce2066c3b7abad48484a14f1b1b3ede2daad5
MarionetkaX <sergey07061999@gmail.com>
2022-04-01 11:32:33 +0300

    fix: update migration

---
+++ migrations/versions/a2d0f4fd41c5_table.py
@@ -0,0 +1,25 @@
+"""Table
+
+Revision ID: a2d0f4fd41c5
+Revises: 
+Create Date: 2022-04-01 11:31:28.896689
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel
+
+
+# revision identifiers, used by Alembic.
+revision = 'a2d0f4fd41c5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
