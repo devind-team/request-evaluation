201b47f2d1eabb3893e58f4b6b4a778dca535608
MarionetkaX <sergey07061999@gmail.com>
2022-04-18 16:14:47 +0300

    feat: added task to send statistics every day at 8 00

---
+++ task.py
@@ -0,0 +1,39 @@
+import aioschedule as schedule
+from asyncio import get_event_loop
+from sqlalchemy.future import select
+from datetime import date, timedelta
+from time import sleep
+from generate_word.generate_file import create_report
+from send_message.send_email import send_file
+from database import engine
+from models import Traffic
+from settings import CONFIG_EMAIL
+from os import remove
+
+
+async def send_message():
+    async with engine.connect() as session:
+        get_record = (await session.execute(
+            select(Traffic).
+            where(Traffic.create_at == date.today() - timedelta(days=1)))).first()
+    path_report = create_report(get_record.counter,
+                                round(get_record.average_load, 2),
+                                round(get_record.maximum_load, 2)
+                                )
+    await send_file(CONFIG_EMAIL['MAIL_FROM'],
+                    CONFIG_EMAIL['MAIL_PASSWORD'],
+                    CONFIG_EMAIL['MAIL_FROM'],
+                    CONFIG_EMAIL['MAIL_TO'],
+                    path_report,
+                    CONFIG_EMAIL['MAIL_SERVER'],
+                    CONFIG_EMAIL['MAIL_PORT']
+                    )
+    remove(path_report)
+    sleep(1)
+
+schedule.every().day.at('08:00').do(send_message)
+# schedule.every(30).seconds.do(send_message)
+
+while True:
+    get_event_loop().run_until_complete(schedule.run_pending())
+    sleep(1)
