89e4c7c129bd96b25d8d5aa6b2a6c4e98845ec6e
MarionetkaX <sergey07061999@gmail.com>
2022-04-18 16:11:55 +0300

    fix: change commit

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ engine = create_async_engine(
 
 async def init_db():
     async with engine.begin() as conn:
-        # await conn.run_sync(SQLModel.metadata.drop_all)
+        await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
 
 
