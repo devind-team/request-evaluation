b953553e30e9b2cf9f2e57c882f5a4333ffb6e41
MarionetkaX <sergey07061999@gmail.com>
2022-04-29 11:38:44 +0300

    fix: task execution time changed

--- task.py
+++ task.py
@@ -31,7 +31,7 @@ async def send_message():
     remove(path_report)
     sleep(1)
 
-schedule.every().day.at('11:00').do(send_message)
+schedule.every().day.at('8:00').do(send_message)
 # schedule.every(30).seconds.do(send_message)
 
 while True:
