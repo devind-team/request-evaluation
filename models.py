9267b03b96c472cdf422a85cb6ff8761603dcfc9
MarionetkaX <sergey07061999@gmail.com>
2022-05-19 13:05:32 +0300

    feat: добавлен новый функционал для нескольких сайтов

--- models.py
+++ models.py
@@ -6,9 +6,10 @@ from sqlalchemy import Date, Column
 
 class TrafficBase(SQLModel):
     counter: int = Field(default=1, title='Количество запросов в день')
-    average_load: float = Field(nullable=True, title='Средняя нагрузка на сеть за день')
-    maximum_load: float = Field(nullable=True, title='Максимальная нагрузка на сеть за день')
-    create_at: date = Field(sa_column=Column('create_at', Date, unique=True), title='Дата')
+    average_load: Optional[float] = Field(title='Средняя нагрузка на сеть за день')
+    maximum_load: Optional[float] = Field(title='Максимальная нагрузка на сеть за день')
+    create_at: date = Field(title='Дата')
+    site_id: int = Field(default=None, foreign_key='site.id')
 
 
 class Traffic(TrafficBase, table=True):
@@ -17,3 +18,14 @@ class Traffic(TrafficBase, table=True):
 
 class TrafficCreate(TrafficBase):
     pass
+
+
+class SiteBase(SQLModel):
+    # identification: str = Field(sa_column=Column('identification', unique=True), title='Индентификатор сайта')
+    # site_name: str = Field(sa_column=Column('site_name', unique=True), title='URL сайта')
+    identification: str = Field(title='Индентификатор сайта')
+    site_name: str = Field(title='URL сайта')
+
+
+class Site(SiteBase, table=True):
+    id: int = Field(default=None, primary_key=True)
